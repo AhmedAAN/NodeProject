04c25b3cc9e2ac50b883a6cf725253bcbb232f92
AhmedAAN <ahmed.a.nagy@outlook.com>
2024-01-20 20:51:43 +0200

    second commit

--- app.js
+++ app.js
@@ -16,12 +16,15 @@ app.set('view engine', 'ejs');
 
 const dburi = 'mongodb+srv://ahmednagy:test123@cluster0.jnhdmvn.mongodb.net/meals?retryWrites=true&w=majority';
 
-mongoose.connect(dburi, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then((result) => {
-        console.log("conneted to the database");
-        httpServer.listen(port, () => {
-            console.log(`Server running on localhost:${port}`)
-        })
-    })
-    .catch((err) => { console.log(err) });
+//mongoose.connect(dburi, { useNewUrlParser: true, useUnifiedTopology: true })
+//    .then((result) => {
+//        console.log("conneted to the database");
+//        httpServer.listen(port, () => {
+//            console.log(`Server running on localhost:${port}`)
+//        })
+//    })
+//    .catch((err) => { console.log(err) });
 
+httpServer.listen(4000, () => {
+    console.log(`Server running on localhost:4000`)
+})
